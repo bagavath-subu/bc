bd4a28479ea740429ccc86a2468800cb89636b80
subramanian <subramanian>
2020-03-16 01:35:47 +0530

    fav

--- src/Components/HomePage/Card.jsx
+++ src/Components/HomePage/Card.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import '../../Style/Card.css'
 export default function Card(props) {
-    const { img, name,email } = props
-    return (
-        // <div classNameName="Card">
-        //     <img classNameName="img" src={img} alt="" width="auto" height="250px" />
-        //     <h1>{name}</h1>
-        // </div>
-<>
-        <div align="center" className="fond">
-  
-  
-  <div className="carreaux_presentation_light" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/173024/jonathan_larradet_FLATDESIGN_250x250.png)'}}>
-    <div className="shadow_swhow_mini">
-        <div className="deroul_titre">{name}</div>
-        <div className="deroul_soustitre">{email}</div>
-    </div>
-  </div>
-  </div>
-  </>
-    )
+  const { img, name, email } = props
+  return (
+    // <div classNameName="Card">
+    //     <img classNameName="img" src={img} alt="" width="auto" height="250px" />
+    //     <h1>{name}</h1>
+    // </div>
+    <>
+      <div align="center" className="fond">
+
+
+        <div className="carreaux_presentation_light" style={{ backgroundImage: `url(${img})` }}>
+          <div className="shadow_swhow_mini">
+            <div className="deroul_titre">{name}</div>
+            <div className="deroul_soustitre">{email}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
